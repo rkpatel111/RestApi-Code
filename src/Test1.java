47b17544e8b6b09a30ca0c1e1b08811ddacdcc00
Ranjit Patel <Ranjit Patel@DESKTOP-QV2OPJD>
2020-07-09 23:56:33 +0530

    test commint

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		IntStream.range(1, 10).forEach(System.out::print);
+		
+		
+		
+		
+		
+	}
+
+}
